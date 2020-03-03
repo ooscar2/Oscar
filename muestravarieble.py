67007b80b88d1480d723e33a647b9a5d29e19c0c
ooscar2 <ooscar.pg@gmail.com>
2020-03-03 17:33:27 +0000

    muestra variable

---
+++ muestravarieble.py
@@ -0,0 +1,6 @@
+# Declara una variable, y la muestra.
+
+def  main ():
+  #con el "def" se declara una variable que realizará una acción al momento de imprimirla, en esete caso se imprimira la variable "txt"
+  txt = "Hola mundo, con variable"
+  imprimir ( txt )
