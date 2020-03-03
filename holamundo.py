feb8f8c790340a0a6ff03f3fc6973b822f23655f
ooscar2 <ooscar.pg@gmail.com>
2020-03-03 17:01:54 +0000

    Initial commit

---
+++ holamundo.py
@@ -0,0 +1,6 @@
+#Se utiliza el "def" para definir una función, a la cual se le colocara un nombre, en este caso "imprimir"
+def imprimir():
+#al momento de definirla se le agregara la accion que debe realizar en este caso imprimir un mensaje
+  print("HOLA MUNDOOOOOO, ya se usar python!!:)")
+#se imprime el mensaje con la funcion definida
+print (imprimir())
---
+++ main.py
@@ -0,0 +1,10 @@
+# Programa 00
+# Programa principal de repl.it: todos los programas se ejecutan como
+# función de este programa,
+# Referir de la siguiente manera:
+# de nombreprogramasinextensiónpy import nombrefunciónprincipaldelprograma
+# nombrefunciónprincipaldelprograma () -> Le llamaremos main (), para 
+# homólogo con otros idiomas de programación, como C #
+
+de  funciones  import  main
+main ()
